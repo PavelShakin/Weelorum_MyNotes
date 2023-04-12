66746dc12f19e26847586df2fe0542288bd458ec
Pavlo Shakin <shakin@weelorum.com>
2023-04-12 12:56:50 +0300

    * created base SplashFragment.kt

--- app/src/main/java/com/mynotes/android/screens/splash/SplashFragment.kt
+++ app/src/main/java/com/mynotes/android/screens/splash/SplashFragment.kt
@@ -4,11 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.mynotes.core.common.GlobalConstraints.emptyString
 import com.mynotes.core.views.BaseFragment
 import com.mynotes.resources.themes.AppTheme
+import com.mynotes.resources.themes.MyNotesTheme
 
 class SplashFragment : BaseFragment() {
 
@@ -32,6 +42,29 @@ class SplashFragment : BaseFragment() {
 
     @Composable
     private fun Body() {
+        Box(
+            modifier = Modifier
+                .background(MyNotesTheme.colors.primaryColor),
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            Column(
+                Modifier.fillMaxHeight(),
+                verticalArrangement = Arrangement.SpaceAround,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Spacer(modifier = Modifier.size(10.dp))
+                SplashLogoImageView()
+                Spacer(modifier = Modifier.size(10.dp))
+            }
+        }
+    }
 
+    @Composable
+    private fun SplashLogoImageView() {
+        Image(
+            contentScale = ContentScale.FillWidth,
+            painter = painterResource(com.mynotes.resources.R.drawable.ic_logo),
+            contentDescription = emptyString
+        )
     }
 }
---
+++ core/src/main/java/com/mynotes/core/common/GlobalConstraints.kt
@@ -0,0 +1,5 @@
+package com.mynotes.core.common
+
+object GlobalConstraints {
+    const val emptyString = ""
+}
